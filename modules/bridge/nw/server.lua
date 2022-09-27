34a2e5c9c25c93106e415b9f291089bec90d2982
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-09-27 18:33:40 +0200

    feat(modules/bridge/nw/server): newwheel support

---
+++ modules/bridge/nw/server.lua
@@ -0,0 +1,15 @@
+local playerDropped = ...
+local Inventory
+
+CreateThread(function()
+	Inventory = server.inventory
+end)
+
+AddEventHandler('NW:PlayerLogout', playerDropped)
+
+AddEventHandler('NW:PlayerDataUpdated', function(source, data)
+	local inventory = Inventory(source)
+	if not inventory then return end
+    local groups = type(data.groups) == 'string' and {[data.groups] = 0} or data.groups
+	inventory.player.groups = groups
+end)
