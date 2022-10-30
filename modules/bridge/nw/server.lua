823cdd6002a47c1206cd83fba34d733e32ebbf54
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-10-30 12:12:01 +0100

    fix(modules/bridge): don't update when player is not created

--- modules/bridge/nw/server.lua
+++ modules/bridge/nw/server.lua
@@ -9,7 +9,9 @@ AddEventHandler('NW:PlayerLogout', playerDropped)
 
 AddEventHandler('NW:PlayerDataUpdated', function(source, data)
 	local inventory = Inventory(source)
-	if not inventory then return end
-    local groups = type(data.groups) == 'string' and {[data.groups] = 0} or data.groups
+
+	if not inventory or not inventory.player then return end
+
+	local groups = type(data.groups) == 'string' and {[data.groups] = 0} or data.groups
 	inventory.player.groups = groups
 end)
